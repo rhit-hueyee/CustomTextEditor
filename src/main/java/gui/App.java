a2b13f10cc8d5f5b8fa09410ed4c926bd7f3f3ed
ehuey275 <hueyee@rose-hulman.edu>
2023-07-14 16:45:47 -0400

    Checkstyle stop bitching

--- src/main/java/gui/App.java
+++ src/main/java/gui/App.java
@@ -1,4 +1,4 @@
-package GUI;
+package gui;
 
 public class App {
     public static void main(String[] args){
--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -1,6 +1,4 @@
-package GUI;
-
-import GUI.TextEdit;
+package gui;
 
 public class Main {
     public static void main(String[] args){
