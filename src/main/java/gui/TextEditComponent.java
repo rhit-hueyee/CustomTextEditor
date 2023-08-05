593c0a1e784077cd89116e6ed3ac7d53803e2f64
ehuey275 <hueyee@rose-hulman.edu>
2023-08-05 19:03:24 -0400

    textEdit wireframe

--- src/main/java/gui/TextEditComponent.java
+++ src/main/java/gui/TextEditComponent.java
@@ -5,11 +5,23 @@ import java.awt.*;
 
 public class TextEditComponent extends JPanel {
     public TextEditComponent(String text){
-        super(true);
-        JLabel filler = new JLabel(text);
-        filler.setHorizontalAlignment(JLabel.CENTER);
-        this.setLayout(new GridLayout(1, 1));
-        this.add(filler);
+        super();
+        //Text Editing Menu
+        //Text Editor -> this will probably have to be a class that extends JTextComponent, for now, I'm doing a JTextArea
+        JMenu menu = new JMenu();
+        JMenuItem menuItem = new JMenuItem();
+        menuItem.setText("Item 1");
+        menu.add(menuItem);
+
+        this.add(menu);
+        JTextArea textArea = new JTextArea("balls", 100, 100);
+        this.add(textArea);
+
+
+//        JLabel filler = new JLabel(text);
+//        filler.setHorizontalAlignment(JLabel.CENTER);
+//        this.setLayout(new GridLayout(1, 1));
+//        this.add(filler);
     }
 
 }
