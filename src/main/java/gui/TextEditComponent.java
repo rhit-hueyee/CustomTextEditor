d2f6506fb7d2e5a5d72feb78717d30c1b5e660cf
ehuey275 <hueyee@rose-hulman.edu>
2023-08-06 15:41:51 -0400

    Toolbar Scaffolding

--- src/main/java/gui/TextEditComponent.java
+++ src/main/java/gui/TextEditComponent.java
@@ -1,18 +1,26 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TextEditComponent extends JPanel {
 
 
     public TextEditComponent(){
-        super();
+        super(new BorderLayout());
         /* Text Editing Menu ->
         This is where cut, copy, paste, redo, undo, find, replace, zoom in, zoom out, reset zoom,
-        show all characters, toggle line wrap, toggle line number, toggle indent display, ece tera live
+        show all characters, toggle line wrap, toggle line number, toggle indent display, etcetera live
+        This will be done with a JToolBar
          */
+        JToolBar toolbar = new JToolBar("Tools");
+        addToolBarButtons(toolbar);
+        //set preferred size
+        //add toolbar to layout
+        //add scroll pane? (not sure if needed)
+
         JMenuBar textEditMenuBar = new JMenuBar();
-        JMenu textEditMenu = new JMenu();
+        JMenu textEditMenu = new JMenu("Edit");
         JMenuItem menuItemFile = new JMenuItem("File");
         textEditMenu.add(menuItemFile);
         textEditMenuBar.add(textEditMenu);
@@ -30,5 +38,21 @@ public class TextEditComponent extends JPanel {
         JTextArea textArea = new JTextArea("balls", super.getWidth(), super.getHeight());
         this.add(textArea);
     }
+
+    protected void addToolBarButtons(JToolBar toolbar){
+
+
+        //create first button
+        JButton button = makeToolbarButton();
+
+        toolbar.add(button);
+    }
+
+    protected JButton makeToolbarButton() {
+       return new JButton();
+    }
+
+
+
 
 }
